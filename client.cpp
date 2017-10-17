ad381c9733bf282d08c570f75fc045378929baeb
mhilmiasyrofi <mhilmiasyrofi@gmail.com>
2017-10-17 19:32:11 +0700

    add checksum

--- client.cpp
+++ client.cpp
@@ -11,6 +11,19 @@ char data[1024];
 struct sockaddr_in serverAddr;
 socklen_t addr_size;
 
+char compute_checksum(char* str) {
+    char sum = 0x0;
+
+    int length = sizeof (str) / sizeof (char);
+    length--;
+    length--;
+    for (int i = 0; i < length; i++) {
+        sum += str[i];
+    }
+
+    return sum;
+}
+
 void sendSegment(char c, int seqNum) {
 
     char segment[9];
@@ -23,7 +36,7 @@ void sendSegment(char c, int seqNum) {
     segment[5] = 0x2;
     segment[6] = c;
     segment[7] = 0x3;
-    segment[8] = 0x0;
+    segment[8] = compute_checksum(segment);
 
     /*Send message to server*/
     sendto(clientSocket,segment, 9, 0, (struct sockaddr *)&serverAddr, addr_size);
@@ -40,56 +53,57 @@ int getSeqNum(char byte[7]) {
     return n;
 }
 
-int main(){
+int main(int argc, char* argv[]){
 
-  /*Create UDP socket*/
-  clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
+    /*Create UDP socket*/
+    clientSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-  /*Configure settings in address struct*/
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(7891);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    /*Configure settings in address struct*/
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(7891);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*Initialize size variable to be used later on*/
-  addr_size = sizeof serverAddr;
+    /*Initialize size variable to be used later on*/
+    addr_size = sizeof serverAddr;
 
-  char recv_data[7];
+    char recv_data[7];
 
-  while(1){
-    printf("Type a sentence to send to server:\n");
-    fgets(data,1024,stdin);
-    printf("You typed: %s",data);
+    while(1){
+        printf("Type a sentence to send to server:\n");
+        fgets(data,1024,stdin);
+        printf("You typed: %s",data);
 
-    // nBytes = strlen(data) + 1;
-    int seqNum = 0;
-    int LAR = 0; //last ACK received
-    int WINDOW_SIZE = 4;
-    while (seqNum < strlen(data)) {
-        int i = LAR;
-        for (i; i < LAR + WINDOW_SIZE; i++) {
-            sendSegment(data[i], i);
-            printf("%c", data[i]);
+        // nBytes = strlen(data) + 1;
+        int seqNum = 0;
+        int LAR = 0; //last ACK received
+        int WINDOW_SIZE = 4;
+        while (seqNum < strlen(data)) {
+            int i = LAR;
+            for (i; i < LAR + WINDOW_SIZE; i++) {
+                sendSegment(data[i], i);
+                // printf("%c", data[i]);
+            }
+
+            memset(recv_data, 0x0, sizeof recv_data);
+            /*Receive message from server*/
+            nBytes = recvfrom(clientSocket,recv_data, 7, 0, NULL, NULL);
+
+            // printf("%02x\n", compute_checksum(recv_data));
+            for (int i = 0; i < 7; i++)
+                printf("%02x ", recv_data[i]);
+
+            printf("%02x ", compute_checksum(recv_data));
+            fflush(stdout);
+
+            printf("\n");
+
+            if (compute_checksum(recv_data) == recv_data[6]) { //cek checksum
+                seqNum = getSeqNum(recv_data);
+                LAR = seqNum;
+            }
         }
-
-        memset(recv_data, 0x0, sizeof recv_data);
-        /*Receive message from server*/
-        nBytes = recvfrom(clientSocket,recv_data, 7, 0, NULL, NULL);
-        // printf("%d", nBytes);
-        for (int i = 0; i < 7; i++)
-            printf("%02x ", recv_data[i]);
-        printf("\n");
-
-        seqNum = getSeqNum(recv_data);
-        LAR = seqNum;
-
-        // printf("Received from server: %s\n",recv_data);
-
-
     }
 
-
-  }
-
-  return 0;
+    return 0;
 }
--- server.cpp
+++ server.cpp
@@ -14,6 +14,19 @@ struct sockaddr_in serverAddr, clientAddr;
 struct sockaddr_storage serverStorage;
 socklen_t addr_size, client_addr_size;
 
+char compute_checksum(char* str) {
+    char sum = 0x0;
+
+    int length = sizeof (str) / sizeof (char);
+    length--;
+    length--;
+    for (int i = 0; i < length; i++) {
+        sum += str[i];
+    }
+
+    return sum;
+}
+
 void sendACK(int nextSeqNum) {
     char segment[7];
     memset(segment,0x0, sizeof segment);
@@ -23,7 +36,7 @@ void sendACK(int nextSeqNum) {
     segment[2] = (nextSeqNum & 0x00ff0000) >> 16;
     segment[1] = (nextSeqNum & 0xff000000) >> 24;
     segment[5] = 0x0;
-    segment[6] = 0x0;
+    segment[6] = compute_checksum(segment);
 
     /*Send uppercase message back to client, using serverStorage as the address*/
     sendto(udpSocket, segment, 7, 0, (struct sockaddr *)&serverStorage, addr_size);
@@ -41,65 +54,68 @@ int getSeqNum(char byte[7]) {
     return n;
 }
 
-int compute_checksum(char str[7]) {
-    
-}
+int main(int argc, char* argv[]){
+    int i, nextSeqNum = 0; //nextSeqNum = Largest Acceptable Frame
+    int WINDOW_SIZE = 4;
+    char arr[WINDOW_SIZE];
+    memset(arr, 0x0, sizeof arr);
 
-int main(){
-  int i, nextSeqNum = 0; //nextSeqNum = Largest Acceptable Frame
-  int WINDOW_SIZE = 4;
-  char arr[WINDOW_SIZE];
-  memset(arr, 0x0, sizeof arr);
+    /*Create UDP socket*/
+    udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-  /*Create UDP socket*/
-  udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+    /*Configure settings in address struct*/
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(7891);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  /*Configure settings in address struct*/
-  serverAddr.sin_family = AF_INET;
-  serverAddr.sin_port = htons(7891);
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
+    /*Bind socket with address struct*/
+    bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
 
-  /*Bind socket with address struct*/
-  bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+    /*Initialize size variable to be used later on*/
+    addr_size = sizeof serverStorage;
 
-  /*Initialize size variable to be used later on*/
-  addr_size = sizeof serverStorage;
+    // printf("c");
 
-  // printf("c");
+    while(1){
+        /* Try to receive any incoming UDP datagram. Address and port of
+          requesting client will be stored on serverStorage variable */
+        nBytes = recvfrom(udpSocket, data, 9,0,(struct sockaddr *)&serverStorage, &addr_size);
 
-  while(1){
-    /* Try to receive any incoming UDP datagram. Address and port of
-      requesting client will be stored on serverStorage variable */
-    nBytes = recvfrom(udpSocket, data, 9,0,(struct sockaddr *)&serverStorage, &addr_size);
+        // for (int x = 0; x < 9; x++)
+        //     printf("%02x ", data[x]);
+        // printf("\n");
 
-    if (nextSeqNum < getSeqNum(data) + 1) { //Jika segment yang diterima lebih dari Largest Acceptable Frame
+        if (nextSeqNum < getSeqNum(data) + 1) { //Jika segment yang diterima lebih dari Largest Acceptable Frame
+            if (compute_checksum(data) == data[8]) { // cek error dengan checksum
 
-        //memasukkan karakter yang terbaca ke array dengan size WINDOW_SIZE
-        arr[getSeqNum(data) - nextSeqNum] = data[6];
+                //memasukkan karakter yang terbaca ke array dengan size WINDOW_SIZE
+                arr[getSeqNum(data) - nextSeqNum] = data[6];
 
-        //cek Largest Acceptable Frame
-        for (i = 0; i < WINDOW_SIZE && arr[i] != 0x0; i++) {
+                //cek Largest Acceptable Frame
+                for (i = 0; i < WINDOW_SIZE && arr[i] != 0x0; i++) {
+                }
+                for (int j = 0; j < i ;j++) {
+                    printf("%c", arr[j]);
+                }
+
+                fflush(stdout);
+
+                //copy karakter yang belum terbaca pada sisa WINDOW
+                char temp[WINDOW_SIZE];
+                strcpy(temp, arr);
+                memset(arr, 0x0, sizeof arr);
+                for (int j = i ; j < WINDOW_SIZE; j++) {
+                    arr[j-i] = arr[j];
+                }
+
+                // update LAF
+                nextSeqNum += i;
+
+                sendACK(nextSeqNum);
+            }
         }
-        for (int j = 0; j < i ;j++) {
-            printf("%c", arr[j]);
-        }
-        fflush(stdout);
-
-        //copy karakter yang belum terbaca pada sisa WINDOW
-        char temp[WINDOW_SIZE];
-        strcpy(temp, arr);
-        memset(arr, 0x0, sizeof arr);
-        for (int j = i ; j < WINDOW_SIZE; j++) {
-            arr[j-i] = arr[j];
-        }
-
-        // update LAF
-        nextSeqNum += i;
-
-        sendACK(nextSeqNum);
     }
-  }
 
-  return 0;
+    return 0;
 }
